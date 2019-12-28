1648daa632b06238658e2a76a1159bdae08a6545
ACER <ACER@LAPTOP-SFUSB99U>
2019-12-28 20:52:38 +0700

    Quản lý sản phẩm

---
+++ QuanLySanPham/src/tonhi/com/test/TestSanPham.java
@@ -0,0 +1,12 @@
+package tonhi.com.test;
+
+import tonhi.com.model.SanPham;
+
+public class TestSanPham {
+
+	public static void main(String[] args) {
+		SanPham coca =new SanPham("SP1","Coca",15,2);
+		System.out.println(coca);
+	}
+
+}
